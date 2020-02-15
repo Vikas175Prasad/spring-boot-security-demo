81ae3c9973f0a2aebe60ac59422b1371a9f79868
vikas <vikas@HP-2000-Notebook-PC>
2020-02-15 12:58:46 +0530

    first commit

---
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -0,0 +1,67 @@
+package com.example.demo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import static com.example.demo.security.ApplicationUserRole.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import com.example.demo.student.Student;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	private final PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+		
+		this.passwordEncoder = passwordEncoder;
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http
+			.authorizeRequests()
+			.antMatchers("/","index","/css/*","/js/*")
+			.permitAll()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.httpBasic();
+	}
+
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {// Determines how we get users details
+
+		UserDetails jamesGordonUser = User.builder()
+				.username("james")
+				.password(passwordEncoder.encode("james")) // Must be encrypted using BCrypt
+				.roles(STUDENT.name()) //Internally stored as ROLE_STUDENT
+				.build();
+		
+		
+		UserDetails vikasUser = User.builder()
+				.username("vikas")
+				.password(passwordEncoder.encode("vikas")) 
+				.roles("ADMIN") 
+				.build();
+		
+		return new InMemoryUserDetailsManager(
+					
+						jamesGordonUser,
+						vikasUser
+				);
+	}
+
+}
---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,30 @@
+package com.example.demo.student;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+	private static final List<Student> STUDENTS = Arrays.asList(
+			new Student(1,"Jimmy"),
+			new Student(2,"James Gordon"),
+			new Student(3,"James Neesham")
+			
+			);
+	
+	@GetMapping(path= "{studentId}")
+	public Student getStudent(@PathVariable("studentId") Integer studentId) {
+		
+		return STUDENTS.stream()
+				.filter(student -> studentId.equals(student.getStudentId()))
+				.findFirst()
+				.orElseThrow(() -> new IllegalStateException("Student with id = "+ studentId + "not exist"));
+	}
+}
